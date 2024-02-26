40f588350ad0eaf5218a83c98f118fcc8d322972
bogie <bogie.jiffy0y@icloud.com>
2024-02-26 12:59:57 +0100

    more searchResponse infos

--- src/search.rs
+++ src/search.rs
@@ -1,13 +1,57 @@
 use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
+use crate::error::ApiErr;
 use crate::RequestImpl;
+use crate::ApiErrorType;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct SearchResponse {
     pub manga_id: String,
     pub titles: HashMap<String, Vec<String>>,
     pub tags: Vec<String>,
+    pub status: Status,
+    pub ext: String,
+    pub number: u32
+}
+#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
+pub enum Status {
+    Dropped,
+    Hiatus,
+    Ongoing,
+    Completed,
+    Upcoming
+}
+
+impl TryFrom<u64> for Status {
+    type Error = ApiErr;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Dropped),
+            1 => Ok(Self::Hiatus),
+            2 => Ok(Self::Ongoing),
+            3 => Ok(Self::Completed),
+            4 => Ok(Self::Upcoming),
+            _ => Err(ApiErr {
+                message: Some("Couldnt find manga status".to_string()),
+                cause: None,
+                err_type: ApiErrorType::InternalError,
+            })
+        }
+    }
+}
+
+impl From<Status> for u64 {
+    fn from(value: Status) -> Self {
+        match value {
+            Status::Dropped => 0,
+            Status::Hiatus => 1,
+            Status::Ongoing => 2,
+            Status::Completed => 3,
+            Status::Upcoming => 4,
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
